ce9d4e785d2383e253e9b8abe559e23a1a86db5e
Savecoders <pablopincay2003@gmail.com>
2025-11-09 17:37:23 -0500

    feat: initial appConfig and useController example

--- App/src/main/java/gym/vitae/Main.java
+++ App/src/main/java/gym/vitae/Main.java
@@ -1,24 +1,22 @@
 package gym.vitae;
 
 import gym.vitae.controller.EmpleadosController;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import gym.vitae.core.ApplicationConfig;
+import gym.vitae.model.enums.EstadoCliente;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("gym_vitaePU"); // usa tu persistence-unit
-        EntityManager em = emf.createEntityManager();
+  public static void main(String[] args) {
 
-        EmpleadosController empleadosController = new EmpleadosController();
-        System.out.println("Lista de empleados:");
-        empleadosController.getEmpleados().forEach(empleado ->
-                System.out.println(empleado.getNombres() + " " + empleado.getApellidos()));
+    ApplicationConfig.init();
 
+    EmpleadosController empleadosController = new EmpleadosController();
+    System.out.println("Lista de empleados:");
+    empleadosController
+        .getEmpleados()
+        .forEach(
+            empleado -> System.out.println(empleado.getNombres() + " " + empleado.getApellidos()));
 
-
-    }
+    System.out.println(EstadoCliente.ACTIVO);
+  }
 }
