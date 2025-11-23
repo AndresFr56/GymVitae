6f03c7c01bcb2ccfb702f14fd1aa74384d05dd00
Savecoders <pablopincay2003@gmail.com>
2025-11-23 00:52:41 -0500

    feat(main): refactor Main class to extend JFrame and improve application initialization

--- App/src/main/java/gym/vitae/Main.java
+++ App/src/main/java/gym/vitae/Main.java
@@ -1,22 +1,45 @@
 package gym.vitae;
 
-import gym.vitae.controller.EmpleadosController;
+import com.formdev.flatlaf.FlatClientProperties;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
+import com.formdev.flatlaf.util.FontUtils;
 import gym.vitae.core.ApplicationConfig;
-import gym.vitae.model.enums.EstadoCliente;
+import gym.vitae.views.common.ViewManager;
+import gym.vitae.views.components.Menu;
+import gym.vitae.views.utils.Preferences;
+import raven.modal.Drawer;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+  public Main() {
+    init();
+  }
 
   public static void main(String[] args) {
-
+    // cargar configuraciones y repositorios
     ApplicationConfig.init();
 
-    EmpleadosController empleadosController = new EmpleadosController();
-    System.out.println("Lista de empleados:");
-    empleadosController
-        .getEmpleados()
-        .forEach(
-            empleado -> System.out.println(empleado.getNombres() + " " + empleado.getApellidos()));
+    // iniciar las preferencias
+    Preferences.init();
 
-    System.out.println(EstadoCliente.ACTIVO);
+    // configurar el tema path resources/themes/*
+    FlatRobotoFont.install();
+    FlatLaf.registerCustomDefaultsSource("themes");
+    UIManager.put("defaultFont", FontUtils.getCompositeFont(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
+    Preferences.setupLaf();
+    EventQueue.invokeLater(() -> new Main().setVisible(true));
+  }
+
+  private void init() {
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    getRootPane().putClientProperty(FlatClientProperties.FULL_WINDOW_CONTENT, true);
+    Drawer.installDrawer(this, Menu.getInstance());
+    ViewManager.install(this);
+    setSize(new java.awt.Dimension(1366, 768));
+    setLocationRelativeTo(null);
   }
 }
