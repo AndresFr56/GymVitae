ec2cce54a6cb87708b5fa0bf406533f014d3083d
Savecoders <pablopincay2003@gmail.com>
2025-11-24 21:59:21 -0500

    fix(dependencies): not find swing-pack in snapshot

---
+++ App/src/test/java/gym/vitae/controller/PersonalControllerTest.java
@@ -0,0 +1,21 @@
+package gym.vitae.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PersonalControllerTest {
+
+    @BeforeEach
+    void setUp() {
+      }
+
+    @AfterEach
+    void tearDown() {
+      }
+
+    @Test
+    void getEmpleados() {
+      }
+}
