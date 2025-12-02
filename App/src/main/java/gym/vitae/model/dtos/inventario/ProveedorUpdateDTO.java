e58c4f2ddf0aa441c555b221d12c16ce0a34635b
AndresFr56 <andresfreirech56@outlook.com>
2025-12-02 12:27:45 -0500

    refactor(DTOs): ProveedorUpdateDTO remove code from supplier update constructor

--- App/src/main/java/gym/vitae/model/dtos/inventario/ProveedorUpdateDTO.java
+++ App/src/main/java/gym/vitae/model/dtos/inventario/ProveedorUpdateDTO.java
@@ -1,5 +1,8 @@
 package gym.vitae.model.dtos.inventario;
 
+/**
+ * DTO para la actualización de proveedores.
+ */
 public class ProveedorUpdateDTO {
 
   private String codigo;
@@ -10,7 +13,34 @@ public class ProveedorUpdateDTO {
   private String direccion;
   private Boolean activo;
 
-  public ProveedorUpdateDTO() {}
+  /**
+   * Constructor por defecto para instanciar la clase.
+   */
+  public ProveedorUpdateDTO() {
+  }
+
+  /**
+   * Constructor para actualizar datos del proveedor. El código del proveedor no es modificable, el
+   * estado no se puede cambiar al actualizar, solo al dar de baja
+   *
+   * @param nombre del proveedor
+   * @param contacto del proveedor
+   * @param telefono del proveedor
+   * @param email del proveedor
+   * @param direccion del proveedor
+   */
+  public ProveedorUpdateDTO(
+      String nombre,
+      String contacto,
+      String telefono,
+      String email,
+      String direccion) {
+    this.nombre = nombre;
+    this.contacto = contacto;
+    this.telefono = telefono;
+    this.email = email;
+    this.direccion = direccion;
+  }
 
   public String getCodigo() {
     return codigo;
